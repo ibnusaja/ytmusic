c118bf15e013d7a09d5dc756b923e78fdd1408fa
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2022-03-14 17:02:18 +0800

    Update music.sh

--- data/adb/service.d/music.sh
+++ data/adb/service.d/music.sh
@@ -2,4 +2,5 @@
 while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
 sleep 1
 chcon u:object_r:apk_data_file:s0 /data/adb/Music/base.apk
+pathYtmusic=$(pm path com.google.android.apps.youtube.music | awk -F 'package:' '{print $2}') && sleep 1
 mount -o bind /data/adb/Music/base.apk $pathYtmusic
