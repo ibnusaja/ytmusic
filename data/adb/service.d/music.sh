29ca37ccbcf5990494e991acc1ce1360ccba15bb
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2022-03-14 17:00:17 +0800

    Update music.sh

--- data/adb/service.d/music.sh
+++ data/adb/service.d/music.sh
@@ -2,4 +2,4 @@
 while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
 sleep 1
 chcon u:object_r:apk_data_file:s0 /data/adb/Music/base.apk
-mount -o bind /data/adb/Music/base.apk /data/app/com.google.android.apps.youtube.music-lCSsddLGy5mCNrXjoRJmlg==/base.apk
+mount -o bind /data/adb/Music/base.apk $pathYtmusic
